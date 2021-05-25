409b4f626f12a40f4ac8c13cb361be7f55ee0f91
Vitalii Sosluik <soslukven@gmail.com>
2021-05-25 20:19:32 +0300

    Task 4. Goroutines. Practice mode.

--- main.go
+++ main.go
@@ -4,10 +4,26 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strconv"
+	"sync"
 )
 
 func main() {
-	requestUrl := "https://jsonplaceholder.typicode.com/posts/"
+	countPost(100)
+}
+
+func countPost(num int) {
+	wg := new(sync.WaitGroup)
+	for i := 0; i < num; i++ {
+		go post(strconv.Itoa(i), wg)
+	}
+	wg.Wait()
+}
+
+func post(id string, wg *sync.WaitGroup) {
+	wg.Add(1)
+	defer wg.Done()
+	requestUrl := "https://jsonplaceholder.typicode.com/posts/" + id
 
 	resp, err := http.Get(requestUrl)
 	if err != nil {
@@ -17,6 +33,5 @@ func main() {
 	if errRead != nil {
 		panic(errRead)
 	}
-	fmt.Print(string(bytes))
-
+	fmt.Println(string(bytes))
 }
